3334f3add75bb3c959a34f845124df4daa089354
Tim Rupp <caphrim007@gmail.com>
2013-02-05 10:47:27 -0600

    Initial import of 1.0 from February 2007

---
+++ config.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# Default nickname for the bot
+nickname = 'mylittleircbot22'
+
+# Server to run phenny on
+hosts = [
+	#'irc.freenode.net:6667',
+	'us.undernet.org:6667',
+	#'irc.dal.net',
+	#'irc.prison.net'
+]
+
+# Domains to 'who' users for
+who_for_users = [
+	'*.host.com',
+	'172.168.*'
+]
+
+exempt_from_reporting = [
+	'mylittleircbot22',		# This is the irc bot
+]
